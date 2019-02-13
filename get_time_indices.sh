98d07a9a7d176a2c4182ad1d9faa318121b48d92
Yixiang <yg5d6@mail.missouri.edu>
2019-02-12 18:29:16 -0600

    add scripts to extract ctm from speeches

--- get_time_indices.sh
+++ get_time_indices.sh
@@ -1,1 +1,2 @@
-steps/get_train_ctm.sh data/train data/lang exp/tri ctm
+steps/get_train_ctm.sh data/train data/lang exp/tri1 ctm/train
+steps/get_ctm_fast.sh data/test data/lang exp/tri1/decode ctm/test
